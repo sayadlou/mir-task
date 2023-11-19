bbf954213528aec0c0be68c779f6e44e4cd060b2
saeid sayadlou <s.sayadlou@gmail.com>
2023-11-19 11:29:38 +0330

    add nginx config file and system demon file for server

--- app/config/settings/production.py
+++ app/config/settings/production.py
@@ -1,10 +1,8 @@
 import logging
 
 import sentry_sdk
-from sentry_sdk.integrations.celery import CeleryIntegration
 from sentry_sdk.integrations.django import DjangoIntegration
 from sentry_sdk.integrations.logging import LoggingIntegration
-from sentry_sdk.integrations.redis import RedisIntegration
 
 from .base import *
 from .base import env
@@ -104,8 +102,6 @@ sentry_logging = LoggingIntegration(
 integrations = [
     sentry_logging,
     DjangoIntegration(),
-    CeleryIntegration(),
-    RedisIntegration(),
 ]
 sentry_sdk.init(
     dsn=SENTRY_DSN,
